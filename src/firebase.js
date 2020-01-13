348748f40c381b34b1c99954a08521bfbae3abc5
carina092 <carina@carinascheiberlich.nl>
2020-01-13 12:18:34 +0100

    ENV configuration

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/analytics';
 import 'firebase/database';
 
 const firebaseConfig = Firebase.initializeApp({
-  apiKey: 'AIzaSyBDYQP-QF8MJhrHTWEkd6pV81YPlwh-KG8',
-  authDomain: 'lush-scents-plu-tool.firebaseapp.com',
-  databaseURL: 'https://lush-scents-plu-tool.firebaseio.com',
-  projectId: 'lush-scents-plu-tool',
-  storageBucket: 'lush-scents-plu-tool.appspot.com',
-  messagingSenderId: '762138444480',
-  appId: '1:762138444480:web:67c6abfa4af0e3c9f22325',
-  measurementId: 'G-DCHTLHHZT7',
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID,
 });
 
 Firebase.analytics();
